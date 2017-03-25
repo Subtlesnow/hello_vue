c600d5eb0da8ba47df7afb138c752e68ac6160fe
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2017-03-25 14:04:50 -0600

    Form complete

---
+++ app.js
@@ -0,0 +1,13 @@
+new Vue({
+
+  // targeting the div with id of 'events'
+
+  el:'#events',
+  // this is for values/data
+  data: {},
+  // this will run app on load
+  ready: function() {},
+  // where methods are registered
+  methods: {}
+
+});
